11d56a3f857684c71c8f3c0e76d80aa04e53fe5d
F-H-Svell <63893907+F-H-Svell@users.noreply.github.com>
2020-05-08 20:11:19 +0600

    Add files via upload

--- korzina.php
+++ korzina.php
@@ -7,6 +7,9 @@ include('data_base.php');
 <!DOCTYPE html>
 <html>
 <head>
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+<link href="/css/korzina_disaign.css" rel="stylesheet">
+
 <script>
 var priceList = {
     "001" : {"id" : "001", "subid" : {}, "name" : "IPhone 5", "price" : "20500"},
@@ -36,6 +39,24 @@ cart.init("basketwidjet", config);
 </head>
 <body>
 <h1>Корзина</h1>
+<div>
+<a href="/delete_korzina.php" onclick="cart.clearBasket()" style="float: right;">Очистить</a>
+
+<a href="#" id="basketwidjet" onclick="cart.showWinow('bcontainer', 1)"></a> <span style="font: normal 11px Arial"></span>
+</div>
+3) Форма заказа
+<div id="order" class="popup">
+<a href="#" onclick="cart.closeWindow('order', 0)" style="float:right"><img src="img/close.png" /></a>
+<h4>Введите ваши контактные данные</h4></p>
+<p><form id="formToSend">
+<input id="fio" type="text" placeholder="Ваши фамилия и имя"  class="" />
+<input id="city" type="text" placeholder="Город"  class=""/>
+<input id="phone" type="text" placeholder="Контактный телефон" class=""/>
+<input id="email" type="text" placeholder="Домашний адрес" class="" />
+</form>
+<a href="/delete_korzina.php"><button onclick="cart.sendOrder('formToSend,overflw,bsum');"  >Отправить заказ</button></a>
+
+</div>
 <?php 
         /*$result = $mysql->query("SELECT 'korzina', 'tovar'
         FROM `korzina`,`tovar`
@@ -50,31 +71,14 @@ cart.init("basketwidjet", config);
 
             $tovar = $tovar->fetch_assoc();
      ?>
-       
-
+       <li class="product-wrapper">
         <p><?=$korzina['tovar_id']; ?></p>
-        <img src="<?=$tovar['image']; ?>" height="250px" class="imga"/> 
-
-		<h3>	<?=$tovar['name']; ?></h3>
-		
-            <p><?=$tovar['price'] . ' COM'; ?></p>    
-        <?php endwhile; ?> 
-<div>
-<span>Корзина: </span>
-<a href="#" onclick="cart.clearBasket()" style="float: right;">Очистить</a>
-<a href="#" id="basketwidjet" onclick="cart.showWinow('bcontainer', 1)"></a> <span style="font: normal 11px Arial"></span>
-</div>
-3) Форма заказа
-<div id="order" class="popup">
-<a href="#" onclick="cart.closeWindow('order', 0)" style="float:right"><img src="img/close.png" /></a>
-<h4>Введите ваши контактные данные</h4></p>
-<p><form id="formToSend">
-<input id="fio" type="text" placeholder="Ваши фамилия и имя"  class="" />
-<input id="city" type="text" placeholder="Город"  class=""/>
-<input id="phone" type="text" placeholder="Контактный телефон" class=""/>
-<input id="email" type="text" placeholder="Электронная почта" class="" />
-</form>
-<button onclick="cart.sendOrder('formToSend,overflw,bsum');" href="#">Отправить заказ</button>
-</div>
+        <h3>	<?=$tovar['name']; ?></h3>
+        <p><?=$tovar['price'] . ' COM'; ?></p>
+       
+       
+        <img src="<?=$tovar['image']; ?>" height="250px" class="imga"/>              
+       </li>
+        <?php endwhile; ?>
 </body>
 </html>
